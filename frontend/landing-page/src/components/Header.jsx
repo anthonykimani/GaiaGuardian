bcb624bbe7f85b9ec492650fec8456a93084e185
Bleyle <Bennybleylle09@gmail.com>
2025-02-09 01:16:36 -0800

    Add files via upload

--- frontend/landing-page/src/components/Header.jsx
+++ frontend/landing-page/src/components/Header.jsx
@@ -37,7 +37,7 @@ const Header = () => {
       }`}
     >
       <div className="flex items-center px-5 lg:px-7.5 xl:px-10 max-lg:py-4">
-        {/* <a className="block w-[12rem] xl:mr-8" href="#hero">
+        <a className="block w-[12rem] xl:mr-8" href="#hero">
           <img
             src={brainwave}
             width={190}
@@ -45,7 +45,7 @@ const Header = () => {
             alt="Brainwave"
             className="pointer-events-none select-none"
           />
-        </a> */}
+        </a>
 
         <nav
           className={`${
